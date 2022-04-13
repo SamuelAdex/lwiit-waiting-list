e72344f792385c3824ebcb9f8caf6f1a94595028
Samuel Adex <adexsamuel6@gmail.com>
2022-04-13 12:03:20 +0100

    minor changes

---
+++ dist/precache-manifest.db6c634e2cdb9774305628ea84db1390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69db8e802151eebffa368476d4b7f336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57fe56914bafc8d38c76",
+    "url": "/static/css/main.1a798fdd.chunk.css"
+  },
+  {
+    "revision": "138068164c188115fd25",
+    "url": "/static/js/2.5af59eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5af59eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fe56914bafc8d38c76",
+    "url": "/static/js/main.ae2340d9.chunk.js"
+  },
+  {
+    "revision": "4136da90a7ec2eb08f98",
+    "url": "/static/js/runtime-main.3e96bb55.js"
+  },
+  {
+    "revision": "0bcc805d55a597a69eebbffb87781f27",
+    "url": "/static/media/pngtree.0bcc805d.jpg"
+  }
+]);
