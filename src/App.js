6c8309b81b8f0ad8f2df63171e406e6fd3ecfba1
Samuel Adex <adexsamuel6@gmail.com>
2022-04-13 11:50:08 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <Header />
       <div className="context">
         <h1>The Loveworld Institute of Innovation and Technology.</h1>
-        <p>We create an enabling environment that stimulates emergence of innovative people, products and platforms for the advancement of the gospel, producing intentional result right from the start. We are a throughbred army of versatile skills with focused based operation for the kingdom and the development of humanity.</p>
+        <p>We create an enabling environment that stimulates emergence of innovative people, products and platforms for the advancement of the gospel, producing intentional result right from the start.</p>
       </div>
       <WaitlistForm />
       <Footer />
